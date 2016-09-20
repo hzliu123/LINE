b5d216928e03b22beaa195a7295f569d3ba6fe39
Joseph Liu <hzliu123@gmail.com>
2016-09-21 03:55:40 +0800

    Use absolute path to fix issues not finding .auth file

--- sendmsg/sendmsg.sh
+++ sendmsg/sendmsg.sh
@@ -2,6 +2,7 @@
 
 export PYTHONPATH=/home/ubuntu/LINE:$PYTHONPATH
 
-cd /home/ubuntu/LINE/sendmsg
+# push and change to the directory this script resides
+pushd `dirname $0`
 python sendmsg.py "$1" "$2" "$3" "$4"
-
+popd
